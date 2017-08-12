f9cd139869ea3a22ae1f337c28af05ed53dca49e
gmf-office <gmf0927@163.com>
2017-08-12 12:50:05 +0800

    优化说明

--- takephoto/src/main/java/com/kwok/takephoto/TakeParam.java
+++ takephoto/src/main/java/com/kwok/takephoto/TakeParam.java
@@ -13,9 +13,8 @@ import java.io.File;
  */
 public class TakeParam {
 
-    public static final String CROP_TYPE = "image/*";
-    public static final String OUTPUT_FORMAT = Bitmap.CompressFormat.JPEG.toString();
-
+    private static final String CROP_TYPE = "image/*";
+    private static final String OUTPUT_FORMAT = Bitmap.CompressFormat.JPEG.toString();
     private static final int DEFAULT_ASPECT = 1;
     private static final int DEFAULT_OUTPUT = 300;
 
@@ -28,33 +27,41 @@ public class TakeParam {
 
     protected Context mContext;
 
-    private String mCropName;
-    private String mCameraName;
+    private String mCropName;   //剪切文件名称
+    private String mCameraName; //拍照文件名称
 
-    protected File mCropFile;
-    protected File mCameraFile;
+    protected File mCropFile;   //剪切文件
+    protected File mCameraFile; //拍照文件
 
-    protected Uri mCropUri;
-    protected Uri mCameraUri;
+    protected Uri mCropUri;     //剪切文件Uri
+    protected Uri mCameraUri;   //拍照文件Uri
+
 
     protected String type;
-    protected String outputFormat;
-    protected String crop;
+    protected String outputFormat;  //输出格式，一般设为Bitmap格式：Bitmap.CompressFormat.JPEG.toString()
+    protected String crop;          //发送裁剪信号
 
-    protected boolean scale;
-    protected boolean returnData;
-    protected boolean noFaceDetection;
 
-    protected int aspectX;
-    protected int aspectY;
+    protected boolean scale;    //是否保留比例
+    protected boolean returnData;   //是否将数据保留在Bitmap中返回
+    protected boolean noFaceDetection;//是否取消人脸识别功能
 
-    public int outputX;
-    public int outputY;
 
-    // 是否需要剪切
+    protected int aspectX; //X方向上的比例
+    protected int aspectY; //Y方向上的比例
+
+
+    public int outputX; //裁剪区的宽
+    public int outputY; //裁剪区的高
+
+    /**
+     * 是否需要剪切 默认为：true
+     */
     public boolean isCrop;
 
-    // 是否压缩
+    /**
+     * 是否需要压缩 默认为：false
+     */
     public boolean isCompress;
 
     public TakeParam(Context context) {
